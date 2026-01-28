33fe57f5139b01970535e0dce815ee94d109c5c8
Vinit-joshi2 <joshivinit20@gmail.com>
2026-01-28 16:42:06 +0530

    Data Tranformation stage

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -5,6 +5,7 @@ import numpy as np
 from imblearn.combine import SMOTEENN
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler,OneHotEncoder , OrdinalEncoder , LabelEncoder
+from sklearn.impute import SimpleImputer
 
 from sklearn.compose import ColumnTransformer
 
@@ -28,9 +29,353 @@ class DataTransformations:
         
 
         try:
-            self.data_ingestion_artifact = data_ingestion_artifact , 
-            self.data_transformation_artifact = data_transformation_config
-            self.data_validation_config = data_validation_artifact
+            self.data_ingestion_artifact = data_ingestion_artifact
+            self.data_transformation_config = data_transformation_config
+            self.data_validation_artifact = data_validation_artifact
+            self._schema_config = read_yaml_file(file_path=SCHEMA_FILE_PATH)
 
         except Exception as e:
             raise MyException(e,sys)
+
+
+    @staticmethod
+    def read_data(file_path) -> pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise MyException(e,sys)
+        
+
+
+
+    def get_data_transformer_object(self) -> Pipeline:
+
+        """
+        Creates and returns a data transformer object for the data, 
+        including gender mapping, dummy variable creation, column renaming,
+        feature scaling, and type adjustments.
+        """
+        logging.info("Entered get_data_transformer_object method of DataTransformation class")
+
+
+
+        try:    
+            numeric_transformer = StandardScaler()
+            logging.info("Tranformation started - StandardScaler")
+
+            num_features = self._schema_config["num_features"]
+            logging.info("Columns loaded from schema")
+
+            # prepricessor pipline
+            preprocessor = ColumnTransformer(
+                transformers=[
+                    ("StandardSclaer" , numeric_transformer , num_features)
+                ],
+                remainder="passthrough"
+            )
+
+            final_pipline = Pipeline(steps=[("Preprocessor" , preprocessor)])
+            logging.info("Final Pipline Ready!!!")
+            logging.info("Exited get_data_transformer_object method of DataTransformation class")
+            return final_pipline
+        
+
+        except Exception as e:
+            logging.exception("Exception occurred in get_data_transformer_object method of DataTransformation class")
+            raise MyException(e,sys)
+        
+
+    def _drop_id_columns(self , df):
+        # Drop the id and _c39 column
+        logging.info("Dropping 'id' and '_c39' columns")
+        drop_col = self.schema_config["drop_columns"]
+        if drop_col in df.columns:
+            df = df.drop(drop_col)
+        
+        return df
+    
+
+    
+    def imputer_Num_cat(data, imputer=None):
+
+
+        # data.drop(columns = "_c39" , inplace = True)
+
+        num_cols = [col for col in data.columns if data[col].dtype in ['int','int64', 'int32', 'float64']]
+
+        logging.info("Imputing NAN value from numric columns")
+
+        if imputer is None:
+            imputer = SimpleImputer(missing_values=np.nan, strategy="median")
+            imputer.fit(data[num_cols])
+
+        data_imputed_numeric = pd.DataFrame(
+            imputer.transform(data[num_cols]),
+            index=data.index,
+            columns=num_cols
+        )
+
+        
+        logging.info("Imputing NAN value from categorical columns")
+        data = data.replace('?', np.nan)
+
+
+        cat_cols = [col for col in data.columns if data[col].dtype == "object"]
+
+        if imputer is None:
+            imputer = SimpleImputer(missing_values=np.nan, strategy="constant" , fill_value="UNKNOWN")
+
+            imputer.fit(data[cat_cols])
+
+        data_imputed_categorical = pd.DataFrame(
+            imputer.transform(data[cat_cols]),
+            index=data.index,
+            columns=cat_cols
+        )
+
+
+
+        data = pd.concat(
+        [data_imputed_numeric, data_imputed_categorical],
+        axis=1
+        )
+
+        return data
+
+    '''
+
+    def scaling_data(data, scaler=None):
+
+        logging.info("----------------- Scaling Numeric value -------------------")
+        num_cols = data.select_dtypes(include=['int' , 'int64', 'int32', 'float64']).columns.tolist()
+
+
+        if len(num_cols) == 0:
+            raise ValueError("No numeric columns found for scaling")
+
+        if scaler is None:
+            scaler = StandardScaler()
+            scaler.fit(data[num_cols])
+
+        data_scaled = pd.DataFrame(
+            scaler.transform(data[num_cols]),
+            index=data[num_cols].index,
+            columns=num_cols
+        )
+
+        data = pd.concat(
+            [data.drop(columns = data_scaled.columns) , data_scaled],
+            axis = 1
+        )
+
+        return data
+    '''
+    
+    def OHE_cat(data , encoder_col = None , encoder = None) ->pd.DataFrame:
+
+        cat_cols = [col for col in data.columns if data[col].dtype == "object"]
+
+
+        nominal = ['authorities_contacted', 'incident_state', 'insured_hobbies']
+
+        data_ohe = data[nominal]
+
+        logging.info("Aplying One Hot Encoding to Categorical Features")
+
+        if encoder == None:
+            encoder = OneHotEncoder(
+                handle_unknown="ignore" ,
+                drop = "if_binary"
+            )
+
+            encoder.fit(data_ohe)
+            encoder_col = encoder.get_feature_names_out(data_ohe.columns)
+
+
+        data_encoded = encoder.transform(data_ohe).toarray()
+        data_encoded = pd.DataFrame(data_encoded ,
+                                    index = data_ohe.index ,
+                                    columns = encoder_col
+                                    )
+                
+        data = data.drop(columns=nominal)
+        data = pd.concat([data, data_encoded], axis=1)
+
+        return data
+    
+
+    def OE_cat(data, encoder = None) -> pd.DataFrame:
+        cat_cols = [col for col in data.columns if data[col].dtype == "object"]
+
+        ordinal = ['collision_type', 'incident_type', 'incident_severity']
+
+        data_le = data[ordinal]
+
+        collision_type = ['UNKNOWN', 'Side Collision', 'Rear Collision', 'Front Collision']
+        incident_severity = ['Trivial Damage','Minor Damage','Major Damage','Total Loss']
+        incident_type = ['Parked Car','Single Vehicle Collision','Multi-vehicle Collision','Vehicle Theft']
+        
+        logging.info("Aplying Ordinal  Encoding to Categorical Features")
+
+        if encoder == None:
+            # Create object
+            encoder = OrdinalEncoder(categories=[collision_type, incident_type,incident_severity])
+            encoder.fit(data_le)
+
+        ## Transform the data
+        data_encoded = encoder.transform(data_le)
+        data_encoded = pd.DataFrame(data_encoded,
+                                    index = data_le.index,
+                                    columns = data_le.columns)
+
+
+        # Concatenating categorical feature after applying Oridinal Encoding
+        data = data.drop(columns=ordinal)
+        data = pd.concat([data, data_encoded], axis=1)
+
+        return data
+
+
+    def label_encoding(data):
+
+        cat_cols = data.select_dtypes(include=["object"]).columns.tolist()
+
+        logging.info("Applying Label  Encoding to Categorical Features")
+
+        for cat in cat_cols:
+
+            le = LabelEncoder()
+
+            data[cat] = le.fit_transform(data[cat])
+
+        return data
+        
+
+    def initiate_data_transformation(self) -> DataTransformationArtifact:
+
+        """
+        Initiates the data transformation component for the pipeline.
+        """
+
+        try:
+            logging.info("Data Transformation Started !!!")
+            if not self.data_validation_artifact.validation_status:
+                raise Exception(self.data_validation_artifact.message)
+            
+
+
+            # Load train and test data
+
+            train_df = self.read_data(file_path=self.data_ingestion_artifact.trained_file_path)
+            test_df = self.read_data(file_path=self.data_ingestion_artifact.test_file_path)
+            logging.info("Train-test data loaded")
+
+
+            input_feature_train_df = train_df.drop(columns=[TARGET_COLUMN])
+            target_feature_train_df = train_df[TARGET_COLUMN]
+
+            input_feature_test_df = test_df.drop(columns=[TARGET_COLUMN])
+            target_feature_test_df = test_df[TARGET_COLUMN]
+            logging.info("Input and Target cols defined for both train and test df.")
+
+
+            # Apply Customer transformation 
+
+            # Train Data
+            input_feature_train_df = self._drop_id_columns(input_feature_train_df)
+            input_feature_train_df = self.imputer_Num_cat(input_feature_train_df)
+            input_feature_train_df = self.OHE_cat(input_feature_train_df)
+            input_feature_train_df = self.OE_cat(input_feature_train_df)
+            input_feature_train_df = self.label_encoding(input_feature_train_df)
+
+            # Test Data
+            input_feature_test_df = self._drop_id_columns(input_feature_test_df)
+            input_feature_test_df = self.imputer_Num_cat(input_feature_test_df)
+            input_feature_test_df = self.OHE_cat(input_feature_test_df)
+            input_feature_test_df = self.OE_cat(input_feature_test_df)
+            input_feature_test_df = self.label_encoding(input_feature_test_df)
+
+            logging.info("Custom transformations applied to train and test data")
+
+            logging.info("Starting data transformation")
+            preprocessor = self.get_data_transformer_object()
+            logging.info("Got the preprocessor object")
+
+            logging.info("Initializing transformation for Training-data")
+            input_feature_train_arr = preprocessor.fit_transform(input_feature_train_df)
+
+            logging.info("Initializing transformation for Testing-data")
+            input_feature_test_arr = preprocessor.transform(input_feature_test_df)
+
+            logging.info("Transformation done end to end to train-test df.")
+
+
+            logging.info("Applying SMOTEENN for handling imbalanced dataset.")
+
+            smt = SMOTEENN(sampling_strategy="minority")
+            input_feature_train_final, target_feature_train_final = smt.fit_resample(
+                input_feature_train_arr, target_feature_train_df
+            )
+            input_feature_test_final, target_feature_test_final = smt.fit_resample(
+                input_feature_test_arr, target_feature_test_df
+            )
+            logging.info("SMOTEENN applied to train-test df.")
+
+
+
+            train_arr = np.c_[input_feature_train_final, np.array(target_feature_train_final)]
+            test_arr = np.c_[input_feature_test_final, np.array(target_feature_test_final)]
+            logging.info("feature-target concatenation done for train-test df.")
+
+
+            save_object(self.data_transformation_config.transformed_object_file_path , preprocessor)
+            save_numpy_array_data(self.data_transformation_config.transformed_train_file_path , array=train_arr)
+            save_numpy_array_data(self.data_transformation_config.transformed_test_file_path , array=test_arr)
+
+            logging.info("Saving transformation object and transformed files.")
+
+            logging.info("Data transformation completed successfully")
+
+
+            return DataTransformationArtifact(
+                transformed_object_file_path=self.data_transformation_config.transformed_object_file_path,
+                transformed_train_file_path=self.data_transformation_config.transformed_train_file_path,
+                transformed_test_file_path=self.data_transformation_config.transformed_test_file_path
+            )
+        
+
+
+        except Exception as e:
+            raise MyException(e,sys)
+
+
+
+
+
+            
+
+
+
+
+
+
+
+    
+
+
+    
+
+
+    
+
+
+    
+
+
+        
+
+
+
+        
+
+
